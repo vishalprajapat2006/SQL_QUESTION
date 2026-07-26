1419321c5da3697208f87bccd022343d73795321
Vishal Prajapat <vishalprajapat794@gmail.com>
2026-07-26 10:05:16 -0700

    Time: 721 ms (98.28%), Space: 0B (100%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,11 @@
+SELECT person_name
+FROM (
+    SELECT
+        person_name,
+        turn,
+        SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM Queue
+) q
+WHERE total_weight <= 1000
+ORDER BY total_weight DESC
+LIMIT 1;
